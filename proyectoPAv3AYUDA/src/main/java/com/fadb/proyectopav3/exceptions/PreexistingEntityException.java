84e1d351c1a7b0b25f5dfbb86f3f898a73c6ca44
FDB05 <fadominguezb01@educastillalamancha.es>
2025-01-15 13:26:19 +0100

    Ayuda CREACION METODOS
    una pequeña ayuda para la creacion de metodos

---
+++ proyectoPAv3AYUDA/src/main/java/com/fadb/proyectopav3/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package com.fadb.proyectopav3.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
